3bfb214346e0ce3b83efa58896b7601cb93d30fb
dr96mabuza <dr96mabuza@gmail.com>
2022-03-23 13:37:07 +0200

    only styling left

--- app.js
+++ app.js
@@ -1,45 +1,41 @@
-function form () {
-    const singlePlayButton = document.querySelector('#single-play');
-    const formDisplay = document.querySelector('#form-container');
-    singlePlayButton.addEventListener('click', () => {
-        formDisplay.style.display = 'block';
-    });
-
-    const radio = document.querySelector('input[type=radio]:checked');
-
-    const form = document.querySelector('#form'); //prevents page from reloading
-    form.addEventListener('submit', function (event) {
-        event.preventDefault();
-    });
-
-    const playerName = document.querySelector('.player-name');
-
-    const closeForm = document.querySelector('#close');
-    closeForm.addEventListener('click', () => {formDisplay.style.display = 'none'});
-
-    function formSubmit() {
-        if(playerName.value != '' && radio.value != '') {
-
-
-            formDisplay.style.display = 'none';
-    
-            const inputs = document.querySelectorAll('.icon, #player-name');
-            inputs.forEach(input => {
-                input.value = '';
-            });
-        }
-    }
-
-    const submitButton = document.querySelector('#submit');
-    submitButton.addEventListener('click', () => {
-        formSubmit();
-    });
-}
-
 const gameBoard = () => {
     let myArray = [];
-    let playerName = '';
     
+    const form = () => {
+        const singlePlayButton = document.querySelector('#single-play');
+        const formDisplay = document.querySelector('#form-container');
+        singlePlayButton.addEventListener('click', () => {
+            formDisplay.style.display = 'block';
+        });
+    
+        const radio = document.querySelector('input[type=radio]:checked');
+    
+        const form = document.querySelector('#form'); //prevents page from reloading
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();
+        });
+    
+        const closeForm = document.querySelector('#close');
+        closeForm.addEventListener('click', () => {formDisplay.style.display = 'none'});
+    
+        function formSubmit() {
+            if( radio.value != '') {
+    
+                selection(radio.value);
+                formDisplay.style.display = 'none';
+        
+                const input = document.querySelectorAll('.icon');
+                input.value = '';
+
+            }
+        }
+    
+        const submitButton = document.querySelector('#submit');
+        submitButton.addEventListener('click', () => {
+            formSubmit();
+        });
+    };
+
     const arrayDisplay = () => {
         for(let j = 1; j < 10; j++) {
             let result = '#box-'+j;
@@ -48,16 +44,29 @@ const gameBoard = () => {
         }
     };
 
-    const selection = () => {
+    const selection = (entry) => {
+        let save = entry;
+        
         const buttons = document.querySelectorAll('.game-box');
         buttons.forEach(button => {
             button.addEventListener('click', () => {
-                button.textContent = 'o';
-                arrayPush();
-                result();
+                if (button.textContent === '' && save === 'X'){
+                    button.textContent = save;
+                    arrayPush();
+                    result();
+                    console.log(myArray);
+                    return save = 'O';
+                } else if (button.textContent === '' && save === 'O'){
+                    button.textContent = save;
+                    arrayPush();
+                    result();
+                    console.log(myArray);
+                    return save = 'X';
+                }
             });
+
         });
-    }
+    };
 
     const arrayPush = () => {
         for(let j = 1; j < 10; j++) {
@@ -65,37 +74,49 @@ const gameBoard = () => {
             const box = document.querySelector(result);
             myArray[j-1] = box.textContent;
         }
+        result();
+        arrayDisplay();
+    };
+
+    const result = () => {
+        if (myArray[0] === 'X' && myArray[1] === 'X' && myArray[2] === 'X' ||
+            myArray[3] === 'X' && myArray[4] === 'X' && myArray[5] === 'X' ||
+            myArray[6] === 'X' && myArray[7] === 'X' && myArray[8] === 'X' ||
+            myArray[0] === 'X' && myArray[3] === 'X' && myArray[6] === 'X' ||
+            myArray[1] === 'X' && myArray[4] === 'X' && myArray[7] === 'X' ||
+            myArray[2] === 'X' && myArray[5] === 'X' && myArray[8] === 'X' ||
+            myArray[0] === 'X' && myArray[4] === 'X' && myArray[8] === 'X' ||
+            myArray[6] === 'X' && myArray[4] === 'X' && myArray[2] === 'X') {
+            
+            closing('X');
+
+        } else if (myArray[0] === 'O' && myArray[1] === 'O' && myArray[2] === 'O' ||
+        myArray[3] === 'O' && myArray[4] === 'O' && myArray[5] === 'O' ||
+        myArray[6] === 'O' && myArray[7] === 'O' && myArray[8] === 'O' ||
+        myArray[0] === 'O' && myArray[3] === 'O' && myArray[6] === 'O' ||
+        myArray[1] === 'O' && myArray[4] === 'O' && myArray[7] === 'O' ||
+        myArray[2] === 'O' && myArray[5] === 'O' && myArray[8] === 'O' ||
+        myArray[0] === 'O' && myArray[4] === 'O' && myArray[8] === 'O' ||
+        myArray[6] === 'O' && myArray[4] === 'O' && myArray[2] === 'O') {
+        
+        closing('O');
+
+    }
     }
 
-    const closing = () => {
+    const closing = (winner) => {
         const messageContainer = document.querySelector('#messageContainer');
         messageContainer.style.display = 'block';
         
         const message = document.querySelector('.message');
-        message.textContent = 'you win';
+        message.textContent = 'Congragulations! '+ winner +' is the win!';
 
         const closingButton = document.querySelector('#close-btn');
         closingButton.addEventListener('click', () => {location.reload();})
     }
-    
-    const result = () => {
-        if (myArray[0] === 'o' && myArray[1] === 'o' && myArray[2] === 'o' ||
-            myArray[3] === 'o' && myArray[4] === 'o' && myArray[5] === 'o' ||
-            myArray[6] === 'o' && myArray[7] === 'o' && myArray[8] === 'o' ||
-            myArray[0] === 'o' && myArray[3] === 'o' && myArray[6] === 'o' ||
-            myArray[1] === 'o' && myArray[4] === 'o' && myArray[7] === 'o' ||
-            myArray[2] === 'o' && myArray[5] === 'o' && myArray[8] === 'o' ||
-            myArray[0] === 'o' && myArray[4] === 'o' && myArray[8] === 'o' ||
-            myArray[6] === 'o' && myArray[4] === 'o' && myArray[2] === 'o') {
-            closing();
 
-        }
-    }
-
-    return {arrayDisplay, selection, result};
+    return {form};
 };
 
-const gam = gameBoard();
-gam.arrayDisplay();
-gam.selection();
-gam.result();
+const game = gameBoard();
+game.form();
